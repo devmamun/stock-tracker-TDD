a3bc5b41f3287b96bd626dd07bccbdee7fcb88cf
almamun_techvill <almamun.techvill@gmail.com>
2024-01-01 22:07:06 +0600

    stock for products at retailer

---
+++ app/Models/Product.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name'];
+
+    public function inStock(): bool
+    {
+        return $this->stock()->whereInStock(true)->exists();
+    }
+
+    public function stock()
+    {
+        return $this->hasMany(Stock::class);
+    }
+}
